e866bba28d464db542bcc99b6c808eaa31d1dc8a
Keith Chester <kchester@gmail.com>
2023-01-15 12:04:53 -0800

    feature: trainer for encoder/decoder base

--- birdseye/encoder/train.py
+++ birdseye/encoder/train.py
@@ -8,6 +8,7 @@ from keras.losses import MeanAbsoluteError
 from keras.optimizers import Adam
 
 from birdseye.encoder.dataset import Dataloader
+from birdseye.encoder.model import EncoderDecoder
 
 
 def train_birdseye_encoder(
@@ -20,24 +21,10 @@ def train_birdseye_encoder(
     validation_size: float = 0.25,
     checkpoint_directory: str = "./checkpoints/",
     logdir: str = "./logdir",
-    # load_from_checkpoint: Optional[str] = None
 ):
-    # if load_from_checkpoint is not None:
-    #     model.load_weights(load_from_checkpoint)
-
-    # Can I do all 4 at once?
-    # front_data = Dataloader(batch_size, dataset_folder, "front")
-    # rear_data = Dataloader(batch_size, dataset_folder, "rear")
-    # passenger_side_data = Dataloader(batch_size, dataset_folder, "passenger_side")
-    # driver_side_data = Dataloader(batch_size, dataset_folder, "driver_side")
     data = Dataloader(batch_size, dataset_folder, camera_type)
+    model = EncoderDecoder(encoder, decoder)
 
-    # Validation stuff goes here later
-    encoder_input = Input((256, 256, 3), name="encoder input")
-    encoder_output = encoder(encoder_input)
-    decoder_output = decoder(encoder_output)
-    model = Model(encoder_input, decoder_output, name="encoder/decoder")
-    print(model)
     print(model.summary())
 
     model.compile(optimizer=Adam(learning_rate=0.0005),
@@ -47,7 +34,7 @@ def train_birdseye_encoder(
     # and reload them separately, etc.
     callbacks = [
         ModelCheckpoint(f"{checkpoint_directory}/encoder_decoder.h5",
-                        save_best_only=True),
+                        save_best_only=False),
         BackupAndRestore(backup_dir=f"{checkpoint_directory}/"),
         TensorBoard(log_dir=logdir, histogram_freq=1)
     ]
