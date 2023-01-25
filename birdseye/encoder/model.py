c99fc63e25c0c7b455b4e98d580083a4fbab8de5
keith <kchester@gmail.com>
2023-01-24 21:13:48 -0800

    feature: playing with alt model designs

--- birdseye/encoder/model.py
+++ birdseye/encoder/model.py
@@ -21,11 +21,9 @@ def Encoder():
     input_shape = (256, 256, 3)
     inputs = keras.Input(shape=input_shape)
 
-    outputs = ResNet50(
-        include_top=False,
-        weights="imagenet",
-        input_shape=input_shape
-    )(inputs)
+    outputs = ResNet50(include_top=False, weights="imagenet", input_shape=input_shape)(
+        inputs
+    )
 
     model = keras.Model(inputs=inputs, outputs=outputs, name="encoder")
     return model
@@ -48,11 +46,9 @@ def Decoder():
 
     # Let's build up and outwards
     for filters in [512, 256, 128, 64, 32]:
-        net = Conv2DTranspose(filters, 3, padding="same",
-                              activation="leaky_relu")(net)
+        net = Conv2DTranspose(filters, 3, padding="same", activation="leaky_relu")(net)
         net = BatchNormalization()(net)
-        net = Conv2DTranspose(filters, 3, padding="same",
-                              activation="leaky_relu")(net)
+        net = Conv2DTranspose(filters, 3, padding="same", activation="leaky_relu")(net)
         net = BatchNormalization()(net)
         net = UpSampling2D(2)(net)
 
@@ -64,6 +60,41 @@ def Decoder():
 
     # 3 filters for 3 channels of our output
     outputs = Conv2DTranspose(3, 3, padding="same", activation="tanh")(net)
+
+    model = keras.Model(inputs=inputs, outputs=outputs, name="decoder")
+
+    return model
+
+
+def Decoder2():
+    """
+    This takes the output of the Encoder and attempts to recreate the
+    input.
+    """
+    # The output shape of the ResNet50 top layers is(batch, 8, 8, 2048)
+    input_shape = (8, 8, 2048)
+    inputs = keras.Input(shape=input_shape)
+
+    net = Conv2D(1024, 3, padding="same")(inputs)
+    net = Activation("leaky_relu")(net)
+
+    skip_memory = net
+
+    # Let's build up and outwards
+    for filters in [32, 64, 128, 256]:
+        net = Conv2D(filters, 3, padding="same", activation="leaky_relu")(net)
+        net = Conv2D(filters, 3, padding="same", activation="leaky_relu")(net)
+        net = Conv2D(2 * filters, 3, padding="same", activation="leaky_relu")(net)
+        net = UpSampling2D(2)(net)
+
+        # Handle skip layer
+        skip_memory = UpSampling2D(2)(skip_memory)
+        skip_memory = Conv2D(filters, 1, padding="same")(skip_memory)
+        net = add([net, skip_memory])
+        skip_memory = net
+
+    # 3 filters for 3 channels of our output
+    outputs = Conv2D(3, 3, padding="same", activation="tanh")(net)
 
     model = keras.Model(inputs=inputs, outputs=outputs, name="decoder")
 
@@ -125,13 +156,18 @@ def Critic2():
     inputs = keras.Input((256, 256, 3))
     net = inputs
 
-    for filters in [256, 128, 64, 32, 16]:
-        net = Conv2D(filters, 3, padding="same")(net)
-        net = BatchNormalization()(net)
-        net = Activation("leaky_relu")(net)
-        net = Conv2D(filters, 3, padding="same")(net)
-        net = BatchNormalization()(net)
-        net = Activation("leaky_relu")(net)
+    skip_memory = net
+
+    for filters in [1024, 512, 256, 128, 64, 32, 16]:
+        net = Conv2D(filters, 3, padding="same", activation="leaky_relu")(net)
+        net = Conv2D(filters, 3, padding="same", activation="leaky_relu")(net)
+
+        # Skip layer
+        skip_memory = Conv2D(filters, 1, pading="same", activation="leaky_relu")(
+            skip_memory
+        )
+        net = add([net, skip_memory])
+        skip_memory = net
 
     net = Flatten()(net)
     net = Dense(128, "leaky_relu")(net)
